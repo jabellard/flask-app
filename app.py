2415508def954ca327fcc7fca8232124b8156859
Joe Nathan Abellard <contact@jabellard.com>
2023-09-02 16:43:54 -0400

    Update

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-
-@app.errorhandler(404)
-def error_404(e):
-    return '<h1>No source like your request.</h1>', 404
-
-
 @app.route('/')
-def index():
-    return '<h1>Test app</h1>'
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
-    # Here you can pass any parameters you want.
-    # It will not affect the application work.
+if __name__ == "__main__":
+    app.run(debug=True)
